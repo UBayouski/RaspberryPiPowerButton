d4fded09cb588004fdfac5750ca3fe9da43b3ba1
Vlad <uladzislaubayouski@gmail.com>
2017-09-20 01:56:21 -0700

    Alt GPIO button

--- power_button_alt_gpio.py
+++ power_button_alt_gpio.py
@@ -5,7 +5,11 @@ import subprocess
 
 # Starting up
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(10, GPIO.OUT)
 GPIO.setup(3, GPIO.IN)
+
+# Light up the led button
+GPIO.output(10, True)
 
 # Wait until power button is off
 GPIO.wait_for_edge(3, GPIO.BOTH)
