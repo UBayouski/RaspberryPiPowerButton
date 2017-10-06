31843ea9ca76b280ad9cfbb70567e1359af858ff
Vlad <uladzislaubayouski@gmail.com>
2017-10-06 01:28:15 -0700

    Minor changes

--- power_button.py
+++ power_button.py
@@ -8,6 +8,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(3, GPIO.IN)
 
 # Wait until power button is off
+# Recommended to use GPIO.BOTH for cases with switch
 GPIO.wait_for_edge(3, GPIO.BOTH)
 
 # Shutting down
--- power_button_alt_gpio.py
+++ power_button_alt_gpio.py
@@ -5,13 +5,14 @@ import subprocess
 
 # Starting up
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(10, GPIO.OUT)
+GPIO.setup(23, GPIO.OUT)
 GPIO.setup(3, GPIO.IN)
 
-# Light up the led button
-GPIO.output(10, True)
+# Light up the led button on boot
+GPIO.output(23, True)
 
 # Wait until power button is off
+# Recommended to use GPIO.BOTH for cases with switch
 GPIO.wait_for_edge(3, GPIO.BOTH)
 
 # Shutting down
